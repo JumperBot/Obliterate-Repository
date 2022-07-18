a7bab2779d4bb10eeb86e7f648d40917035b62f3
csg <jumperbotyt@gmail.com>
2022-07-18 20:47:40 +0800

    :sparkles: Preserve-README Option!

--- Obliterator.sh
+++ Obliterator.sh
@@ -9,13 +9,32 @@ git config --global user.name "github-actions[bot]"
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 
 mv .git/ ../
+
+if [ -f "README.md" ]; then
+  readme_file=README.md
+else
+  readme_file=README.*
+fi
+
+if test -f readme_file && $3 ; then
+  mv readme_file ..
+fi
+
 cd ..
 rm -rf workspace/*
 mv .git/ workspace/
+
+if test -f readme_file && $3 ; then
+  mv readme_file workspace/
+fi
+
 cd workspace
 
-git add -A
-git commit -a -m "$2"
-git push --set-upstream origin "$1"
-
-echo "We're done over here!"
+if [ -n "$(git status --porcelain)" ]; then
+  git add -A
+  git commit -a -m "$2"
+  git push --set-upstream origin "$1"
+  echo "We're done over here!"
+else
+  echo "Nothing to commit here!"
+fi
